53ecf7f2e783677ec583a2b658edb75f0a466260
Ivan <ivanmundruku@gmail.com>
2021-02-22 09:56:34 +0300

    Adding frontend Auth logic

--- frontend/src/components/features/auth/Login.js
+++ frontend/src/components/features/auth/Login.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import classnames from 'classnames';
+import {withRouter} from 'react-router-dom';
+import {LoginUser} from './authSlice';
 
 class Login extends Component {
     constructor(props) {
@@ -7,6 +11,7 @@ class Login extends Component {
           
             email:'',
             password:'',
+            errrors:{}
          
         }
     }
@@ -20,14 +25,23 @@ class Login extends Component {
 
     onSubmit(e){
         e.preventDefault()
+        this.props.LoginUser(this.state, this.props.history);
 
-
-        console.log(this.state)
+    }
+  
+    UNSAFE_componentWillReceiveProps(nextProps){
+       
+        if(nextProps.auth.errors){
+            this.setState({
+                errors:nextProps.auth.errors
+            })
+        }
     }
 
 
     render() {
 
+        
         return (
             <div className="login">
                 <div className="container">
@@ -40,7 +54,8 @@ class Login extends Component {
                                     <input type="email" className="form-control form-control-lg" value={this.state.email} placeholder="Email Address" name="email" onChange={(e)=>this.onChange(e)} />
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" className="form-control form-control-lg" value={this.state.password} placeholder="Password" name="password" onChange={(e)=>this.onChange(e)} />
+                                    <input type="password" className={classnames('form-control form-control-lg', { 'is-invalid': this.state.password2 })} value={this.state.password2} placeholder="Confirm Password" name="password" onChange={(e) => this.onChange(e)} />
+                                    {this.state.errors && (<div className="invalid-feedback">{this.state.password}</div>)}
                                 </div>
                                 <input type="submit" className="btn btn-info btn-block mt-4" />
                             </form>
@@ -52,4 +67,11 @@ class Login extends Component {
     }
 }
 
-export default Login;
+
+
+
+const mapStateToProps=(state)=>({
+    auth:state.auth
+})
+
+export default connect(mapStateToProps, {LoginUser})(withRouter(Login));
---
+++ frontend/src/components/features/auth/authSlice.js
@@ -0,0 +1,50 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+export const authSlice=createSlice({
+
+    name:'user',
+    initialState:{
+        user:{},
+        isAuthenticated:false,
+        errors:{}
+    },
+
+    reducers:{
+        loginData:(state, action)=>{
+
+         state.user=action.payload; 
+        },
+         
+        isAuthenticated:(state, action)=>{
+           state.isAuthenticated=action.payload
+        },
+
+        error:(state, action)=>{
+         state.errors=action.payload;
+
+        }
+    }
+
+
+})
+
+export const {loginData, error, isAuthenticated} =authSlice.actions;
+
+export const Registering=(userData)=>dispatch=>{
+
+    dispatch(error(userData));
+
+   
+
+}
+
+export const LoginUser=(userData, history)=>dispatch=>{
+    dispatch(loginData(userData));
+    dispatch(isAuthenticated(true))
+    history.push('/dashBoard');
+
+}
+
+
+export default authSlice.reducer;
